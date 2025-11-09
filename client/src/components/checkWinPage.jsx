8051f0047bbdfc790bdb6ded5406a2d3baf53085
Netanel Nagosa <nagosa1233211@gmail.com>
2025-11-09 23:37:34 +0200

    Fixed ESLint issues and React.createElement in Prize.jsx

--- client/src/components/checkWinPage.jsx
+++ client/src/components/checkWinPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo, useCallback } from 'react'
 import '../styles/checkWin.css'
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
